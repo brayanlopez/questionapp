bb8a38d8a0bec88cb610757288cb58a14c5ec594
BrayanLopez <balg.lg55@gmail.com>
2023-07-31 22:57:39 -0500

    first commit - initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import questions from "../mocks/questions.json";
+import QuestionComponent from "./components/Question.component";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(questions);
+  }, []);
+
+  return (
+    <>
+      {questions.map((question, index) => (
+        <QuestionComponent key={`key-${index}`}>
+          {question.statement}
+        </QuestionComponent>
+      ))}
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Question.component.jsx
@@ -0,0 +1,5 @@
+const QuestionComponent = (props) => {
+  return <p>{props.children}</p>;
+};
+
+export default QuestionComponent;
